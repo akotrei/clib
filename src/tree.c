9215f259a08126a02adbc3c4fec0e8c3dc0ab4a7
Andrei <andrei.kot@innowise-group.com>
2022-06-29 21:56:27 +0300

    tree_improve

--- src/tree.c
+++ src/tree.c
@@ -4,7 +4,6 @@
 #include <string.h>
 #include <assert.h>
 
-
 tree *tree_new(void *obj, int (*compare_fn)(void *obj1, void *obj2))
 {
     tree *t = (tree *)malloc(sizeof(tree));
@@ -19,11 +18,11 @@ void tree_insert(tree *t, void *obj)
     tree *tmp = NULL, *t1 = t, *t2 = t;
     int result = 0;
 
-    while(t != NULL)
+    while (t != NULL)
     {
         result = t->compare_fn(t->data, obj);
 
-        if(result == -1 || result == 0)
+        if (result == -1 || result == 0)
         {
             tmp = t;
             t1 = tmp;
@@ -32,7 +31,7 @@ void tree_insert(tree *t, void *obj)
             tmp = t;
         }
 
-        if(result == 1 || result == 0)
+        if (result == 1 || result == 0)
         {
             tmp = t;
             t1 = tmp;
@@ -42,19 +41,41 @@ void tree_insert(tree *t, void *obj)
         }
     }
 
-    if(result == -1 || result == 0)
+    if (result == -1 || result == 0)
         t1->left = tree_new(obj, t2->compare_fn);
 
-    if(result == 1 || result == 0)
+    if (result == 1 || result == 0)
         t1->right = tree_new(obj, t2->compare_fn);
 }
 
 void tree_print(tree *t, void (*print_fn)(void *obj))
 {
-    if(t == NULL)
+    if (t == NULL)
         return;
 
     tree_print(t->right, print_fn);
     print_fn(t->data);
     tree_print(t->left, print_fn);
 }
+
+/*=========================================================*/
+typedef struct knot_t knot_t;
+
+typedef struct tree
+{
+    int elem_size;
+    int (*cmp_fn)(void *o1, void *o2);
+    void (*dealloc_fn)(void *elem);
+    knot_t* knot; 
+};
+
+
+typedef struct knot_t
+{
+    void* data;
+    knot_t* l;
+    knot_t* r;
+};
+
+static knot_t* knot_create(void* data);
+static void knot_delete(knot_t* knot, void (*dealloc_fn)(void *data));
