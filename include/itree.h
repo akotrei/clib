9afcbf818897a8f88307f7e62d43f76508301833
KotSlava <kot.slava.mih@gmail.com>
2022-07-08 20:04:02 +0300

    tree

--- include/itree.h
+++ include/itree.h
@@ -5,7 +5,13 @@ typedef struct _itree itree;
 
 typedef struct _itree 
 {
-    
+    void* get_data(void* self);
+    itree* get_left(void* self);
+    itree* get_right(void* self);
+    void* add_elem(void* self, void* elem);
+    void* del_elem(void* self, void* elem);
+    void* fnd_elem(void* self, void* elem);    
+    void *self;
 };
 
 #endif
