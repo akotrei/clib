d09cf49ebe7cc1c793db8aa473fa4c1dc356761d
KotSlava <kot.slava.mih@gmail.com>
2022-09-04 20:52:08 +0300

    hash table

--- include/hash_table/hash_table.h
+++ include/hash_table/hash_table.h
@@ -6,56 +6,59 @@
 #include <stddef.h>
 #include "interface.h"
 
-/* 
+/*
+ *
  */
-hash_table_t* 
+hash_table_t*
 hash_table_create(int table_size,
                   uint64_t (*hash_fn)(const void *),
                   int (*compare_fn)(void *, void *),
-                  void* (*copy_fn)(void *),
-                  void (*dealloc_fn)(void *),
+                  void* (*key_copy_fn)(void *),
+                  void* (*val_copy_fn)(void *),
+                  void (*key_dealloc_fn)(void *),
+                  void (*val_dealloc_fn)(void *),
                   int factor,
                   iallocator_t *iallocator);
 
 /*
  */
-void 
+void
 hash_table_delete(hash_table_t *ht);
 
 /*
  */
-void 
-hash_table_insert(hash_table_t *ht, 
-                  void *key, 
+void
+hash_table_insert(hash_table_t *ht,
+                  void *key,
                   void *value);
 
 /*
  */
 void*
-hash_table_search(hash_table_t *ht, 
+hash_table_search(hash_table_t *ht,
                   void *key);
 
 /*
  */
 void*
-hash_table_remove(hash_table_t *ht, 
+hash_table_remove(hash_table_t *ht,
                   void *key);
 
 /*
  */
-void 
-hash_table_factor(hash_table_t *ht, 
+void
+hash_table_factor(hash_table_t *ht,
                   int factor);
 
 /*
  */
-void 
+void
 hash_table_rehashing(hash_table_t *ht);
 
 /*
  */
 void
-hash_table_print(hash_table_t *ht, 
+hash_table_print(hash_table_t *ht,
                  void (*print_fn)(void *o));
 
 #endif
